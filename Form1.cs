add7b06d18542e9092f8d7399e79c9b890db7699
salemdahel <salemdahel123@gmail.com>
2024-05-21 17:07:01 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator_assesment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c;
+            if (radioButton1.Checked)
+            {
+                c = a + b;
+                label4.Text = "Total=" + c.ToString();
+
+            }
+            if (radioButton2.Checked)
+            {
+                c = a - b;
+                label4.Text = "Subtract=" + c.ToString();
+            }
+            if (radioButton3.Checked)
+            {
+                c = a * b;
+                label4.Text = "Multiply=" + c.ToString();
+            }
+            if (radioButton4.Checked)
+            {
+                c = a / b;
+                label4.Text = "Divide=" + c.ToString();
+            }
+            if (radioButton5.Checked)
+            {
+                c = a % b;
+                label4.Text = "Mode=" + c.ToString();
+            }
+        }
+    }
+}
